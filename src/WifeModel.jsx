758a4ba9b0d60c2ffd23ed5e1f55f19b7bbf03c5
Dalinte <kollekcionerssd1@gmail.com>
2023-05-18 20:54:08 +0500

    add github pages deploy, fix base route, add deploy scripts

--- src/WifeModel.jsx
+++ src/WifeModel.jsx
@@ -2,7 +2,7 @@ import {useLoader} from "@react-three/fiber";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
 export default function WifeModel() {
-    const {scene} = useLoader(GLTFLoader, '/wife-model.glb')
+    const {scene} = useLoader(GLTFLoader, '/wife/wife-model.glb')
 
     return (
         <mesh>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wife', // route по названию репозитория для заливки на github pages
 })

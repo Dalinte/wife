c0ed1767d46acf9c930d57180d3d22cc6c652a26
Dalinte <kollekcionerssd1@gmail.com>
2023-05-18 22:09:11 +0500

    remove console

--- src/components/Background/Background.jsx
+++ src/components/Background/Background.jsx
@@ -2,7 +2,7 @@ import { useTexture } from "@react-three/drei"
 
 export default function Background () {
     const colorMap = useTexture('/wife/room-background.jpg')
-    console.log(colorMap)
+
     return (
         <mesh position={[0, 1, -1]}>
             <planeGeometry args={[3, 3]}/>

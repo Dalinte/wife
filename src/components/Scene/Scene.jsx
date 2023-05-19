eeda0fa19ec811474b9ed266a867b17749360235
Dalinte <kollekcionerssd1@gmail.com>
2023-05-19 17:19:28 +0500

    add DirectionalLightHelper

--- src/components/Scene/Scene.jsx
+++ src/components/Scene/Scene.jsx
@@ -1,7 +1,24 @@
 import {Canvas} from "@react-three/fiber";
+import { Environment, OrbitControls, useHelper } from "@react-three/drei";
+import {useRef} from "react";
+import {DirectionalLightHelper} from "three";
+
 import WifeModel from "../WifeModel/WifeModel.jsx";
 import Room from "../Room/Room.jsx";
-import { Environment, OrbitControls } from "@react-three/drei";
+
+const Light = () => {
+    const dirLight = useRef(null);
+    useHelper(dirLight, DirectionalLightHelper, 0.5);
+
+    return (
+        <directionalLight
+            position={[-2, 3, 0]}
+            color={"#ffa700"}
+            intensity={2}
+            ref={dirLight}
+        />
+    );
+};
 
 export default function Scene () {
     return (
@@ -10,10 +27,7 @@ export default function Scene () {
                 <WifeModel/>
                 <Room/>
                 <ambientLight intensity={0.1} />
-                <directionalLight
-                    color="#ffa700"
-                    position={[-1, 2, 0.7]}
-                />
+                <Light/>
             </group>
 
             <OrbitControls/>

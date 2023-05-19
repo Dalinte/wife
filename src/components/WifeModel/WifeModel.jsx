eff1a7fbb1dccd59b9486d16ce57d6115d6eb040
Dalinte <kollekcionerssd1@gmail.com>
2023-05-19 21:49:39 +0500

    replace wife model

--- src/components/WifeModel/WifeModel.jsx
+++ src/components/WifeModel/WifeModel.jsx
@@ -2,13 +2,12 @@ import {useLoader} from "@react-three/fiber";
 import {GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
 export default function WifeModel() {
-    const {scene} = useLoader(GLTFLoader, '/wife/wife-model.glb')
+    const {scene} = useLoader(GLTFLoader, '/wife/wife-model-2.glb')
 
     return (
         <mesh>
             <primitive
                 position={[0.1, 0.15, 0.2]}
-                rotation-y={Math.PI}
                 object={scene}
             />
         </mesh>

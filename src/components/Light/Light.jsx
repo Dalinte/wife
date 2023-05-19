4c61a287386efd41fd44cff3eca0c58427f59c9a
Dalinte <kollekcionerssd1@gmail.com>
2023-05-19 18:35:52 +0500

    add gui lib, add gui for light

--- src/components/Light/Light.jsx
+++ src/components/Light/Light.jsx
@@ -1,16 +1,34 @@
 import {useRef} from "react";
 import {useHelper} from "@react-three/drei";
 import {DirectionalLightHelper} from "three";
+import { useControls } from 'leva'
 
 export default function Light () {
     const dirLight = useRef(null);
     useHelper(dirLight, DirectionalLightHelper, 0.5);
 
+    const defaultValues = {
+        color: "#ffa700",
+        intensity: 2
+    }
+
+    const {color, intensity} = useControls(
+        'Light',
+        {
+            color: defaultValues.color,
+            intensity: {
+                value: defaultValues.intensity,
+                    min: 0,
+                    max: 10
+                }
+            }
+        )
+
     return (
         <directionalLight
             position={[-2, 2.7, 1.6]}
-            color={"#ffa700"}
-            intensity={2}
+            color={color}
+            intensity={intensity}
             ref={dirLight}
             target-position={[4, 0, -1]}
         />
